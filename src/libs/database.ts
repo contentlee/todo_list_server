98a6623ef42595ec867f46963b2f324a277d17f9
contentguy <gogumag3@naver.com>
2023-10-03 19:52:14 +0900

    fix: db async

--- src/libs/database.ts
+++ src/libs/database.ts
@@ -3,13 +3,14 @@ import { Db, MongoClient } from "mongodb";
 class Database {
   static db: Db;
 
-  static connect(callback: () => void) {
-    MongoClient.connect(process.env.MONGO_URI as string)
-      .then((client) => {
-        Database.db = client.db("todo");
-        callback();
-      })
-      .catch((err) => console.log(err));
+  static async connect(callback: () => void) {
+    try {
+      const client = await MongoClient.connect(process.env.MONGO_URI as string);
+      Database.db = client.db("chat");
+      callback();
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
